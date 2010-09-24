f36234fcbcf8f1018809c6a12e7ec45b6491fb6a
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-09-23 22:49:41 -0400

    Initial project creation

---
+++ src/main/java/im/jeanfrancois/etsmaps/App.java
@@ -0,0 +1,13 @@
+package im.jeanfrancois.etsmaps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
