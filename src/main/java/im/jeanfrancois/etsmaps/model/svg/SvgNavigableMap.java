397a4d60de41fcdd12f59a3698e750dc354b254d
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-16 15:23:43 -0400

    Added dummy getRouteBetweenLandmarks implementation

--- src/main/java/im/jeanfrancois/etsmaps/model/svg/SvgNavigableMap.java
+++ src/main/java/im/jeanfrancois/etsmaps/model/svg/SvgNavigableMap.java
@@ -2,6 +2,7 @@ package im.jeanfrancois.etsmaps.model.svg;
 
 import im.jeanfrancois.etsmaps.model.Landmark;
 import im.jeanfrancois.etsmaps.model.NavigableMap;
+import im.jeanfrancois.etsmaps.model.Route;
 
 import java.util.List;
 
@@ -15,6 +16,11 @@ import java.util.List;
  */
 public class SvgNavigableMap implements NavigableMap {
 	public List<? extends Landmark> getLandmarks() {
+		// TODO Implement this method
+		throw new RuntimeException("Unimplemented method!");
+	}
+
+	public Route getRouteBetweenLandmarks(Landmark origin, Landmark destination) {
 		// TODO Implement this method
 		throw new RuntimeException("Unimplemented method!");
 	}
