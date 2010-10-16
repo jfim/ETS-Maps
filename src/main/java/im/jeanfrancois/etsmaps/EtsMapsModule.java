2ca8ea7fa93a4b3147b7d37d09170d5de84890ab
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-16 15:09:21 -0400

    Added javadoc

--- src/main/java/im/jeanfrancois/etsmaps/EtsMapsModule.java
+++ src/main/java/im/jeanfrancois/etsmaps/EtsMapsModule.java
@@ -1,15 +1,12 @@
-/*
- * Created by IntelliJ IDEA.
- * User: jfim
- * Date: Sep 28, 2010
- * Time: 7:06:42 PM
- */
 package im.jeanfrancois.etsmaps;
 
 import com.google.inject.AbstractModule;
 import im.jeanfrancois.etsmaps.ui.MapDisplayComponent;
 import im.jeanfrancois.etsmaps.ui.svg.SvgMapComponent;
 
+/**
+ * Google Guice module to configure the ETS maps applet.
+ */
 public class EtsMapsModule extends AbstractModule {
 	protected void configure() {
 		bind(MapDisplayComponent.class).to(SvgMapComponent.class);
