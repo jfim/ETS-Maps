8b6f2e6dcbd90a4e195441a44c5a5a6c79006a5c
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-16 18:42:45 -0400

    Added method to get distance from an arbitrary point to the closest point on an edge

--- src/main/java/im/jeanfrancois/etsmaps/model/svg/NavigationEdge.java
+++ src/main/java/im/jeanfrancois/etsmaps/model/svg/NavigationEdge.java
@@ -21,4 +21,31 @@ public class NavigationEdge {
 	public NavigationNode getSecond() {
 		return second;
 	}
+
+	public float getSquaredDistanceFromPoint(float pointX, float pointY) {
+		// http://local.wasp.uwa.edu.au/~pbourke/geometry/pointline/
+		// u = ((x3-x1)(x2-x1) + (y3-y1)(y2-y1)) / ((x2-x1)(x2-x1)+(y2-y1)(y2-y1))
+		float dx21 = second.getX() - first.getX();
+		float dy21 = second.getY() - first.getY();
+		float dx31 = pointX - first.getX();
+		float dy31 = pointY - first.getY();
+
+		float u = ((dx31 * dx21) + (dy31 * dy21)) / ((dx21 * dx21) +
+				(dy21 * dy21));
+
+		// Clamp to [0;1]
+		if (u < 0.0f) {
+			u = 0.0f;
+		} else if (1.0f < u) {
+			u = 1.0f;
+		}
+
+		float closestX = first.getX() + (u * dx21);
+		float closestY = first.getY() + (u * dy21);
+
+		float dx = closestX - pointX;
+		float dy = closestY - pointY;
+
+		return (dx * dx) + (dy * dy);
+	}
 }
